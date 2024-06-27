89b1cb7aa1a3402f69660605a94a21eadb70089a
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-06-27 16:06:53 +0200

    Empty project

---
+++ Yummy Book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummy Book
+//
+//  Created by Влад on 6/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummy Book/Yummy_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yummy_BookApp.swift
+//  Yummy Book
+//
+//  Created by Влад on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Yummy_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
