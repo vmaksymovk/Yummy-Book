8e547343c49155a620bf9589704b6de7f75be7c5
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-07-01 22:56:24 +0200

    Search bar added

--- Yummy Book/Views/TabViews/RecipesListView.swift
+++ Yummy Book/Views/TabViews/RecipesListView.swift
@@ -2,26 +2,51 @@ import SwiftUI
 
 struct RecipesListView: View {
     @ObservedObject var viewModel = RecipesViewModel()
-
+    @State private var searchTerm = ""
+    
+    var filteredRecipes: [Recipe] {
+        if searchTerm.isEmpty {
+            return viewModel.recipes
+        } else {
+            return viewModel.recipes.filter { recipe in
+                recipe.name.lowercased().contains(searchTerm.lowercased())
+            }
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(viewModel.recipes) { recipe in
-                NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
-                    HStack {
-                        Image(recipe.imageName)
-                            .resizable()
-                            .frame(width: 70, height: 60)
-                            .cornerRadius(8)
-                        Text(recipe.name)
-                            .font(.title2)
+            VStack {
+                if filteredRecipes.isEmpty {
+                    ContentUnavailableView.search
+                } else {
+                    List(filteredRecipes) { recipe in
+                        NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                            HStack {
+                                if let image = UIImage(named: recipe.imageName) {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .frame(width: 70, height: 60)
+                                        .cornerRadius(8)
+                                } else {
+                                    // Placeholder for missing image
+                                    Rectangle()
+                                        .fill(Color.gray)
+                                        .frame(width: 70, height: 60)
+                                        .cornerRadius(8)
+                                }
+                                Text(recipe.name)
+                                    .font(.title2)
+                            }
+                        }
                     }
                 }
             }
             .navigationTitle("Recipes")
+            .searchable(text: $searchTerm, placement: .automatic, prompt: "Search recipe")
         }
     }
 }
-
 
 #Preview {
     RecipesListView()
